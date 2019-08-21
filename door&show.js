863fbd037421eccbdd55334afdea985058f5ee58
Arthur Melgoza <38125347+arthurmv@users.noreply.github.com>
2019-08-21 11:55:18 -0700

    Add files via upload

---
+++ door&show.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    $("#door1").hover(function() {
+        $("#show1").css("opacity", "1");
+    }, function() {
+        $("#show1").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door2").hover(function() {
+        $("#show2").css("opacity", "1");
+    }, function() {
+        $("#show2").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door3").hover(function() {
+        $("#show3").css("opacity", "1");
+    }, function() {
+        $("#show3").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door4").hover(function() {
+        $("#show4").css("opacity", "1");
+    }, function() {
+        $("#show4").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door5").hover(function() {
+        $("#show5").css("opacity", "1");
+    }, function() {
+        $("#show5").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door6").hover(function() {
+        $("#show6").css("opacity", "1");
+    }, function() {
+        $("#show6").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door7").hover(function() {
+        $("#show7").css("opacity", "1");
+    }, function() {
+        $("#show7").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door8").hover(function() {
+        $("#show8").css("opacity", "1");
+    }, function() {
+        $("#show8").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door9").hover(function() {
+        $("#show9").css("opacity", "1");
+    }, function() {
+        $("#show9").css("opacity", "0");
+    });
+});
+$(document).ready(function() {
+    $("#door10").hover(function() {
+        $("#show10").css("opacity", "1");
+    }, function() {
+        $("#show10").css("opacity", "0");
+    });
+});
